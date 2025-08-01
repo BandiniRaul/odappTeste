e22177ba6a52d5470c9d1f746cd94b23c0778581
BandiniRaul <bandiniraul@gmail.com>
2025-07-31 23:21:53 -0300

    Adicionando mais comentários e funcionalidades

--- server.js
+++ server.js
@@ -27,7 +27,22 @@ app.post('/usuarios', async (req,res) => {
 //Listagem de usuários
 app.get('/usuarios', async (req, res) => {
 
-    const users = await prisma.user.findMany()
+    //filtro de pesquisa usando query params
+    let users = []
+    if(req.query){
+        users = await prisma.user.findMany({
+            where:{
+                name: req.query.name,
+                email: req.query.email,
+                age: req.query.age,
+                city: req.query.city,
+                state: req.query.state,
+                date: req.query.date
+            }
+        })
+    } else {
+        const users = await prisma.user.findMany()
+    }
 
     res.status(200).json(users)
 })
@@ -52,7 +67,7 @@ app.put('/usuarios/:id', async (req,res) => {
     res.status(201).json(req.body)
 })
 
-
+//Deletando usuários
 app.delete('/usuarios/:id', async (req, res) => {
 
     await prisma.user.delete({
@@ -63,16 +78,6 @@ app.delete('/usuarios/:id', async (req, res) => {
 
     res.status(200).json({ message: "Usuário deletado!"})
 })
-
-
-
-
-
-
-
-
-
-
 
 
 //rota para testes
